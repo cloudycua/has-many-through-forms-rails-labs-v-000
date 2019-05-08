fc477f19e6c617078feb7d8376aae7fc01b4a330
Christine Cua <cloudycua@gmail.com>
2019-05-08 22:11:26 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,20 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  # accepts_nested_attributes_for :categories
+  # do not use because of all the custom rules in writer below
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      # only creates category if it has a name
+      if category_attributes[:name].present?
+        category = Category.find_or_create_by(category_attribute)
+        # does not duplicate categories for a post
+        if !self.categories.include?(category)
+          self.categories << category
+        end
+      end
+    end
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
+Category.create(name: "Cool")
+Category.create(name: "Cool2")
+Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+User.create(username: 'RealBigFish')
